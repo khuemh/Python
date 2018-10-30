a8587652e88ea9d7cc2e001c70da550a541c0729
your Name <khuehm@KhueHM.localdomain>
2018-10-30 21:17:38 +0700

    YOLO

--- pygui/src/label.py
+++ pygui/src/label.py
@@ -13,7 +13,7 @@ class App(QWidget):
         self.width = 640
         self.height = 480
         self.init_Window()
-        self.init_Button()
+        self.init_Lable()
         
         
     def init_Window(self):
@@ -21,29 +21,10 @@ class App(QWidget):
         self.setGeometry(self.left, self.top, self.width, self.height)
 
 
-    def init_Button(self):
-        # Init Button 1
-        but01 = QPushButton(self)
-        but01.setText('Left Button')
-        but01.setFlat(True)
-        but01.move(100, 200)
-        but01.clicked.connect(self.but01_clicked)
-
-        # Init Button 2
-        but02 = QPushButton(self)
-        but02.setText('Right Button')
-        but02.resize(100, 32)
-#       but02.setMenu()
-        but02.move(360, 200)
-        but02.clicked.connect(self.but02_clicked)
-
-
-    @pyqtSlot()
-    def but01_clicked(self):
-        print('Left Button')
-    
-    def but02_clicked(self):
-        print('Right Button')
+    def init_Lable(self):
+        lable = QLabel(self)
+        lable.setText('Hello World...!')
+        lable.move(260, 100)
 
 
 if __name__ == '__main__':
---
+++ pygui/src/msgbox.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt5.QtWidgets import * 
+from PyQt5.QtGui import * 
+from PyQt5.QtCore import * 
+
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'Window'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.init_Window()
+        self.init_msgbox() 
+        self.init_button()
+        
+
+    def init_Window(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+
+    def init_button(self):
+        button = QPushButton(self)
+        button.setText('Show message')
+        button.move(100, 200)
+        button.clicked.connect(self.msgshow)
+        self.show()
+
+    
+    def init_msgbox(self):
+        message = QMessageBox.question(self, 'Message', 'Do you want to fuck off?', 
+                                           QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel, 
+                                           QMessageBox.Cancel)
+        if message == QMessageBox.Yes:
+            print('Yes, I want')
+        elif message == QMessageBox.No:
+            print('No, I don\'t')
+        elif message == QMessageBox.Cancel:
+            print('Cancel')
+        self.show()    
+
+    @pyqtSlot()
+    def msgshow(self):
+        init_msgbox()
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()               # Create an App class object
+    sys.exit(app.exec_())
